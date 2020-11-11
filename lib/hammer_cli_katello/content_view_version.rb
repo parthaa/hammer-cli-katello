7004ac7c1c8c355aa25abcc9d8efdc2b440343e2
Partha Aji <paji@redhat.com>
2020-11-11 12:55:54 -0500

    Refs #31117 - Hammer changes to generate metadata json

--- lib/hammer_cli_katello/content_view_version.rb
+++ lib/hammer_cli_katello/content_view_version.rb
@@ -309,6 +309,42 @@ module HammerCLIKatello
       build_options do |o|
         o.expand(:all).including(:content_views, :organizations)
       end
+
+      def execute
+        response = super
+        if option_async? || response != HammerCLI::EX_OK
+          response
+        else
+          @task = load_task(@task['id'])
+          export_history_id = @task["output"]["export_history_id"]
+          export_history = resource.call(:export_histories, :export_history_id => export_history_id)["results"].first if export_history_id
+          if export_history
+            generate_metadata_json(export_history)
+            HammerCLI::EX_OK
+          else
+            output.print_error _("Could not fetch the export history for id = '#{export_history_id}'")
+            HammerCLI::EX_CANTCREAT
+          end
+        end
+      end
+
+      def send_request
+        @task = super
+      end
+
+      private def generate_metadata_json(export_history)
+        metadata_json = export_history["metadata"]
+        begin
+          metadata_path = "#{export_history['path']}/metadata.json"
+          File.write(metadata_path, metadata_json)
+          output.print_message _("Generated #{metadata_path}")
+        rescue SystemCallError
+          t = Tempfile.new("metadata.json")
+          t.write(metadata_json)
+          t.close
+          output.print_message _("Unable to access/write to #{export_history['path']}. Generated #{t.path} instead. You would need this file for importing.")
+        end
+      end
     end
 
     class ExportDefaultCommand < HammerCLIForeman::Command
